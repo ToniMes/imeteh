1ecf29be463849a5bd5cd8fe14bc42665d87a458
ToniMes <116108922+ToniMes@users.noreply.github.com>
2025-04-11 23:17:13 +0200

    Added lever box mesh, defined lever ROM, made the interaction area smaller, and made the lever snap to one of 3 posibble positions on release.

--- Scripts/interaction_object.gd
+++ Scripts/interaction_object.gd
@@ -1,17 +1,17 @@
-#parent class for objects that can be interacted with by the player
+# parent class for objects that can be interacted with by the player
 extends Area3D
 class_name InteractiveObject
 
-var physParent: PhysicsBody3D #the physical object this InteractiveObject is tied to
-var hand: StaticBody3D #the hand currently interacting with this object
+var physParent: PhysicsBody3D # the physical object this InteractiveObject is tied to
+var hand: StaticBody3D # the hand currently interacting with this object
 var isGripped: bool
 
-#assign the physParent
+# assign the physParent
 func _ready():
   physParent = get_parent()
 
 
-#when body enters this area, if it is a hand, assign it
+# when body enters this area, if it is a hand, assign it
 func onHandEnter(body: Node3D):
   if body.name == "Hand":
     hand = body
@@ -19,7 +19,7 @@ func onHandEnter(body: Node3D):
     hand.get_parent().interactionObject = self
 
 
-#break the connection between self and hand on exit
+# break the connection between self and hand on exit
 func onHandExit(body: Node3D):
   if body.name == "Hand":
     if body == hand:
@@ -29,10 +29,10 @@ func onHandExit(body: Node3D):
       isGripped = false
 
 
-#should only be called from hand inside this area
+# should only be called from hand inside this area
 func onTriggerClick():
   isGripped = true
 
-#should only be called from hand inside this area
+# should only be called from hand inside this area
 func onTriggerRelease():
   isGripped = false
