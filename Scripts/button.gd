a7c00d8d0f9e0cf7f70ab359e12bb1cf673e53e6
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2025-05-17 16:49:06 +0200

    Something had broken for me, had to fix it
    - I believe I made some mistake in git, the project would not start for me, but everything works now

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -5,21 +5,21 @@ var button_pressed = false
 func _process(delta):
   # when gripped, button gets pressed down and comes back up
   if isGripped:
-    start_button_press_animation()
+	start_button_press_animation()
 	
 func start_button_press_animation():
 	# skipping animation if button animation is in progress
-    if button_pressed:
-      return
+	if button_pressed:
+	  return
 	
-    button_pressed = true
-    var buttonMesh = $ButtonTopMesh
-    var original_y = buttonMesh.global_position.y
+	button_pressed = true
+	var buttonMesh = $ButtonTopMesh
+	var original_y = buttonMesh.global_position.y
 	
 	# creating button animation that pushes it down slightly and then back up
-    var tween = create_tween()
-    tween.tween_property(buttonMesh, "global_position:y", original_y - 0.01, 0.1)
-    tween.tween_property(buttonMesh, "global_position:y", original_y, 0.1).set_delay(0.15)
-    tween.tween_callback(func(): 
-        button_pressed = false
-    )
+	var tween = create_tween()
+	tween.tween_property(buttonMesh, "global_position:y", original_y - 0.01, 0.1)
+	tween.tween_property(buttonMesh, "global_position:y", original_y, 0.1).set_delay(0.15)
+	tween.tween_callback(func(): 
+		button_pressed = false
+	)
--- Scripts/controler.gd
+++ Scripts/controler.gd
@@ -2,31 +2,14 @@ extends Node
 
 @onready var hand: StaticBody3D = $Hand
 var interactionObject: InteractiveObject
-
-
 func onButtonPressed(name: String):
-<<<<<<< Updated upstream
-  if name == "trigger_click":
-    # if interacting with an object, let the object handle the input
-    if interactionObject != null:
-      interactionObject.onTriggerClick()
-
-
-func onButtonReleased(name: String):
-  if name == "trigger_click":
-    # if interacting with an object, let the object handle the input
-    if interactionObject != null:
-      interactionObject.onTriggerRelease()
-=======
 	if name == "trigger_click":
 	# if interacting with an object, let the object handle the input
 		if interactionObject != null:
 			interactionObject.onTriggerClick()
-
 
 func onButtonReleased(name: String):
 	if name == "trigger_click":
 	# if interacting with an object, let the object handle the input
 		if interactionObject != null:
 			interactionObject.onTriggerRelease()
->>>>>>> Stashed changes
