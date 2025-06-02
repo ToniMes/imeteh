1400a04023aee2832300397fe13780c166259ebd
EmiliaHaramina <148341676+NevenPralas@users.noreply.github.com>
2025-06-02 13:38:30 +0200

    Updated test lever to only be highlighted when user is close enough

--- Scripts/Trolleys/Trolley components/lever.gd
+++ Scripts/Trolleys/Trolley components/lever.gd
@@ -3,8 +3,9 @@ class_name Lever
 
 @onready var targetPosition: Vector3 = position
 @onready var breakingEnabled: bool = true # used to determine if user is allowed to break with the acceleration lever
+@onready var highlightMesh: MeshInstance3D = $PickableObject/XRToolsHighlightVisible/HiglightMesh
+@onready var interactiveLever: InteractiveLever = $PickableObject/InteractiveLever
 var prepared: bool = false
-
 
 var state: int = 0:
   get:
@@ -13,7 +14,18 @@ var state: int = 0:
     state = value
 
 func _process(delta):
+  if interactiveLever and interactiveLever.enabled == true:
+    interactiveLever.enabled = false
+  
   if position.is_equal_approx(targetPosition):
     return
   
   position = position.lerp(targetPosition, delta * 5)
+
+
+func _on_higlight_area_body_entered(body: Node3D) -> void:
+  highlightMesh.visible = true
+
+
+func _on_higlight_area_body_exited(body: Node3D) -> void:
+  highlightMesh.visible = false
