61c20ca9dcb6b0fb340809dbf7c311d90994ff30
Gabrijel-Cobanov <gabrijelcobanov@gmail.com>
2025-06-04 20:35:58 +0200

    Added narrator and subtitles
    - Added narrator scene
    - Added lvl1 voiceline animations that include narration and subtitles that can be toggled on and off via a variable

---
+++ Scripts/narrator.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+# How to use:
+# Each voice line has the following naming scheme:
+# LvlNumber_SequenceCode
+# for example, 1_2 would be the second voiceline in the first level
+# 1_4b would be an alternate voiceline in response to something the player did
+# If a number repeats, for example if there are voicelines 1_2, 1_2a then 1_2a
+# can come after 1_2, but does not have to
+# However, if there are only 1_9a and 1_9b, then only ONE can be played in the
+# sequence of the level
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var subtitles: Label = $Subtitles
+
+
+var enabled_subtitles: bool = true
+
+func _ready():
+  if !enabled_subtitles:
+    subtitles.visible = false
+
+
+func play_voiceline(name: String):
+  animation_player.play(name)
+  
