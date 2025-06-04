457471553c15955a924ef158ef1f357818321ba9
ReflexLevel0 <dominikdejanovic2@gmail.com>
2025-06-04 15:52:15 +0200

    Fixed physics not working for broken lever

--- Scripts/Player/player_controller.gd
+++ Scripts/Player/player_controller.gd
@@ -1,8 +1,12 @@
 extends Node3D
+class_name PlayerController
 
 @onready var left_hand_xr: XRController3D = $XROrigin3D/LeftHandXR
 @onready var right_hand_xr: XRController3D = $XROrigin3D/RightHandXR
-
+@onready var left_hand: XRToolsHand = $XROrigin3D/LeftHandXR/Hand/LeftHand
+@onready var right_hand: XRToolsHand = $XROrigin3D/RightHandXR/Hand/RightHand
+@onready var left_pickup: XRToolsFunctionPickup = $XROrigin3D/LeftHandXR/FunctionPickup
+@onready var right_pickup: XRToolsFunctionPickup = $XROrigin3D/RightHandXR/FunctionPickup
 
 func _ready():
   pass
