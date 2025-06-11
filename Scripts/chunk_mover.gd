bb4085c297ba53dc6bc1e7d99fcd9a4a315a4495
ToniMes <116108922+ToniMes@users.noreply.github.com>
2025-06-11 18:01:34 +0200

    Edited chunk_mover to make movement dependent on lever state and also made movement smoother.

--- Scripts/chunk_mover.gd
+++ Scripts/chunk_mover.gd
@@ -8,35 +8,39 @@ const CHUNK_END = preload("res://Scenes/EnvironmentChunks/Chunk_end.tscn")
 
 @onready var chunk_parent: Node3D = $ChunkParent
 
-var should_move: bool = true
+var should_move: bool = false
 
 var chunks: Array[Node3D]
 
 var chunk_length: int = 50
-var movement_speed: int = 5
-var distance_crossed: int = 0
-var total_distance_crossed: int = 0
+var current_speed: float = 0
+var target_speed: float = 0
+var max_speed: float = 5
 # A class that moves, loads in and loads out chunks of the world
 
 func _ready():
   var chunk1 = CHUNK_1.instantiate()
   var chunk2 = CHUNK_2.instantiate()
+  var chunk3 = CHUNK_2.instantiate()
+  var chunk4 = CHUNK_2.instantiate()
   chunk1.position.z += chunk_length
   chunk2.position.z += chunk_length * 2
+  chunk3.position.z += chunk_length * 3
+  chunk4.position.z += chunk_length * 4
   chunks.append(chunk1)
   chunks.append(chunk2)
+  chunks.append(chunk3)
+  chunks.append(chunk4)
   chunk_parent.add_child(chunk1)
   chunk_parent.add_child(chunk2)
-  
-  distance_crossed = 0
-  total_distance_crossed = 0
+  chunk_parent.add_child(chunk3)
+  chunk_parent.add_child(chunk4)
+  Global.connectGlobalSignal(Global.acc_lever_switched, on_acc_lever)
   
 func _process(delta: float) -> void:
-  if !should_move:
-    return
-  
+  current_speed = lerp(current_speed, target_speed, delta)
   for chunk in chunks:
-    chunk.position.z -= movement_speed * delta
+    chunk.position.z -= current_speed * delta
   
   #print_debug(chunks[0].position.z)
   
@@ -50,9 +54,13 @@ func _process(delta: float) -> void:
       new_chunk = CHUNK_2.instantiate()
     elif r%3 == 2:
       new_chunk = CHUNK_3.instantiate()
-    new_chunk.position.z += chunk_length * 2
+    new_chunk.position.z += chunk_length * 4
     chunk_parent.add_child(new_chunk)
-    distance_crossed = 0
     var chunk_to_remove = chunks.pop_front()
     chunk_parent.remove_child(chunk_to_remove)
     chunks.append(new_chunk)
+
+
+func on_acc_lever(state: bool):
+  target_speed = max_speed if state else 0
+  print(target_speed)
