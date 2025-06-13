abc9c50cabb486523df1760238c5a2bcfe576c4d
ReflexLevel0 <dd54157@fer.hr>
2025-06-13 02:25:17 +0200

    Cleaned up Global.gd

--- autoloads/Global.gd
+++ autoloads/Global.gd
@@ -8,15 +8,8 @@ signal cabinet_door_state_changed(state: DoorState)
 enum TrolleyDirection { LEFT, NONE, RIGHT }
 enum LeverDirectionEnum { LEFT, NONE, RIGHT }
 enum DoorState { OPEN, CLOSED }
-var current_level: int = 0:
-  get:
-    return current_level
-  set(value):
-    current_level = value
-    print("set value to ", current_level)
+var current_level: int = 0
 @export var audio_controller: AudioController = load("res://Scenes/Audio/audio_controller.tscn").instantiate()
-var level_scene: String
-
 
 func _ready() -> void:
   pass
@@ -24,16 +17,14 @@ func _ready() -> void:
 func connectGlobalSignal(sig: Signal, c: Callable):
   connect(sig.get_name(), c)
 
-
 func start_next_level():
   Audio.sfxPlayer.stop_all()
   Audio.musicPlayer.stop_all()
   Audio.narrator.stop_narrator()
   current_level += 1
   start_level(current_level)
-  
-  
+    
 func start_level(level: int = current_level):
   current_level = level
-  level_scene = "res://Scenes/Levels/lvl" + str(current_level) + ".tscn"
+  var level_scene = "res://Scenes/Levels/lvl" + str(current_level) + ".tscn"
   get_tree().change_scene_to_file(level_scene)
