44931b4d3895c06c6886fe7deace10045dd7a8e4
ReflexLevel0 <dd54157@fer.hr>
2025-06-15 17:46:45 +0200

    Added nail hammered sfx

--- Scripts/Trolleys/Trolley components/nail.gd
+++ Scripts/Trolleys/Trolley components/nail.gd
@@ -3,13 +3,10 @@ class_name Nail
 
 @onready var head: RigidBody3D = $Head
 @onready var base = head.position.y
+var last_position
 
 var nailedFlag = true
 signal nailed
-
-
-func _ready():
-  print(base)
 
 
 func _process(delta):
@@ -21,3 +18,8 @@ func _process(delta):
       nailed.emit()
   elif head.position.y > 0:
     head.position.y = 0
+    
+  # Slaying sfx if nail is lower then last time
+  if last_position and head.position.y < last_position.y:
+    Audio.sfxPlayer.play_sound("sfx/hammer_shortened.mp3")
+  last_position = head.position
