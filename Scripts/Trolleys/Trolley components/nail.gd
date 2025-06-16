75ffee2e2dccdaec0ca2d20fb794b58869090f4f
Gabrijel <gabrijelcobanov@gmail.com>
2025-06-16 15:23:24 +0200

    Fixed nail sound not playing

--- Scripts/Trolleys/Trolley components/nail.gd
+++ Scripts/Trolleys/Trolley components/nail.gd
@@ -3,7 +3,6 @@ class_name Nail
 
 @onready var head: RigidBody3D = $Head
 @onready var base = head.position.y
-var last_position
 
 var nailedFlag = true
 signal nailed
@@ -15,11 +14,7 @@ func _process(delta):
     head.position.y = -0.035
     if !nailedFlag:
       nailedFlag = true
+      Audio.sfxPlayer.play_sound("sfx/hammer_shortened.mp3")
       nailed.emit()
   elif head.position.y > 0:
     head.position.y = 0
-    
-  # Slaying sfx if nail is lower then last time
-  if last_position and head.position.y < last_position.y:
-    Audio.sfxPlayer.play_sound("sfx/hammer_shortened.mp3")
-  last_position = head.position
